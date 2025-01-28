d40d0005db81e59a022f686c8ac4fdb5bab9fa15
kimenok <rladpshr2581@gmail.com>
2025-01-28 13:21:21 +0900

    first commit

---
+++ mash/MashKensyu/MashKensyuTests/MashKensyuTests.swift
@@ -0,0 +1,17 @@
+//
+//  MashKensyuTests.swift
+//  MashKensyuTests
+//
+//  Created by eversense on 2025/01/28.
+//
+
+import Testing
+@testable import MashKensyu
+
+struct MashKensyuTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
