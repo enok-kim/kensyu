cbcd3203291ba3161cf99242d03b3ac71394bc75
kimenok <rladpshr2581@gmail.com>
2025-01-30 17:55:28 +0900

    デフォルトのワークアウトカテゴリーを追加

--- mash/MashKensyu/MashKensyu/AppDelegate.swift
+++ mash/MashKensyu/MashKensyu/AppDelegate.swift
@@ -1,36 +1,52 @@
-//
-//  AppDelegate.swift
-//  MashKensyu
-//
-//  Created by eversense on 2025/01/28.
-//
-
 import UIKit
+import RealmSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        let config = Realm.Configuration(
+            schemaVersion: 1,
+            migrationBlock: { migration, oldSchemaVersion in if oldSchemaVersion < 1 {} }
+        )
+        
+        Realm.Configuration.defaultConfiguration = config
+        
+        do {
+            
+            let realm = try Realm()
+            let defaultCategories = ["背中()", "胸(Chest)", "肩＆腕(Sholder&Arm)", "脚(Legs)"]
+            
+            for categoryName in defaultCategories {
+                let existingCategory = realm.objects(WorkoutCategory.self).filter("name = %@", categoryName).first
+                if existingCategory == nil {
+                    
+                    let category = WorkoutCategory()
+                    category.name = categoryName
+                    
+                    try realm.write {
+                        realm.add(category)
+                        
+                    }
+                }
+            }
+            
+        } catch {
+            print("Error initializing Realm: \(error.localizedDescription)")
+        }
+        
         return true
-    }
+        
+    }// Did Finish
 
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+
     }
-
-
 }
-
