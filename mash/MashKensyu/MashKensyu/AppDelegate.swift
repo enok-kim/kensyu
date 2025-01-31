017c3c6332612a0771c589a1ee77d9d77dca6d68
kimenok <rladpshr2581@gmail.com>
2025-01-31 11:12:00 +0900

    AppDelegateリファクタリング

--- mash/MashKensyu/MashKensyu/AppDelegate.swift
+++ mash/MashKensyu/MashKensyu/AppDelegate.swift
@@ -1,41 +1,10 @@
 import UIKit
-import RealmSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
-        let config = Realm.Configuration(
-            schemaVersion: 1,
-            migrationBlock: { migration, oldSchemaVersion in if oldSchemaVersion < 1 {} }
-        )
-        
-        Realm.Configuration.defaultConfiguration = config
-        
-        do {
-            
-            let realm = try Realm()
-            let defaultCategories = ["背中(Back)", "胸(Chest)", "肩＆腕(Sholder&Arm)", "脚(Legs)"]
-            
-            for categoryName in defaultCategories {
-                let existingCategory = realm.objects(WorkoutCategory.self).filter("name = %@", categoryName).first
-                if existingCategory == nil {
-                    
-                    let category = WorkoutCategory()
-                    category.name = categoryName
-                    
-                    try realm.write {
-                        realm.add(category)
-                        
-                    }
-                }
-            }
-            
-        } catch {
-            print("Error initializing Realm: \(error.localizedDescription)")
-        }
-        
+        _ = RealmManager.shared
         return true
         
     }// Did Finish
