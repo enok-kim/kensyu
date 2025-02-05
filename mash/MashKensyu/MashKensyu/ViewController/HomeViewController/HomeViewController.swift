d2eee361698b9c63d3053fe924dd02542f2742c6
kimenok <rladpshr2581@gmail.com>
2025-02-05 09:28:27 +0900

    ボタン関連関数名称変更

--- mash/MashKensyu/MashKensyu/ViewController/HomeViewController/HomeViewController.swift
+++ mash/MashKensyu/MashKensyu/ViewController/HomeViewController/HomeViewController.swift
@@ -17,36 +17,36 @@ class HomeViewController: UIViewController {
     }
     
 
-    @IBAction func onPlan(_ sender: UIButton) {
+    @IBAction func onTapPlanButton(_ sender: UIButton) {
         let planVC = PlanViewController.instantiate()
         navigationController?.pushViewController(planVC, animated: true)
     }
     
-    @IBAction func onAddPlan(_ sender: UIButton) {
+    @IBAction func onTapAddPlanButton(_ sender: UIButton) {
         let addPlanVC = AddPlanViewController.instantiate()
         navigationController?.pushViewController(addPlanVC, animated: true)
     }
     
-    @IBAction func onPlanHistory(_ sender: Any) {
+    @IBAction func onTapPlanHistoryButton(_ sender: UIButton) {
         let planHistoryVC = PlanHistoryViewController.instantiate()
         navigationController?.pushViewController(planHistoryVC, animated: true)
     }
-    @IBAction func onGoogleButton(_ sender: UIButton) {
+    @IBAction func onTapGoogleButton(_ sender: UIButton) {
         let webVC = WebViewController.instantiate(urlString: "http://www.google.com")
         navigationController?.pushViewController(webVC, animated: true)
         }
     
-    @IBAction func onNaverButton(_ sender: UIButton) {
+    @IBAction func onTapNaverButton(_ sender: UIButton) {
         let webVC = WebViewController.instantiate(urlString: "http://www.naver.com")
         navigationController?.pushViewController(webVC, animated: true)
     }
     
-    @IBAction func onYahooButton(_ sender: UIButton) {
+    @IBAction func onTapYahooButton(_ sender: UIButton) {
         let webVC = WebViewController.instantiate(urlString: "http://www.yahoo.co.jp")
         navigationController?.pushViewController(webVC, animated: true)
     }
     
-    @IBAction func onYoutubeButton(_ sender: UIButton) {
+    @IBAction func onTapYoutubeButton(_ sender: UIButton) {
         let webVC = WebViewController.instantiate(urlString: "http://www.youtube.com")
         navigationController?.pushViewController(webVC, animated: true)
     }
